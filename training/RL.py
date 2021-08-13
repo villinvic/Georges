baeb2f33f456b96404ebafcf7f1c80d146c9e625
villinvic <vvillin@hotmail.fr>
2021-08-13 09:34:12 +0200

    testing

--- training/RL.py
+++ training/RL.py
@@ -299,8 +299,13 @@ class AC(tf.keras.Model, Default):
         self.optim.learning_rate.assign(training_params['learning_rate'])
 
         v_loss, mean_entropy, min_entropy, max_entropy, min_logp, max_logp, grad_norm, as_entropy \
-            = self._train(np.float32(training_params['entropy_cost']), np.float32(training_params['gamma']),
-                          np.float32(as_entropy_scale), states, actions, rewards, probs, hidden_states , gpu)
+            = self._train(tf.float32(training_params['entropy_cost']), tf.float32(training_params['gamma']),
+                          tf.float32(as_entropy_scale), tf.convert_to_tensor(states, dtype=tf.float32),
+                          tf.convert_to_tensor(actions, dtype=tf.float32),
+                          tf.convert_to_tensor(rewards, dtype=tf.float32),
+                          tf.convert_to_tensor(probs, dtype=tf.float32),
+                          tf.convert_to_tensor(hidden_states, dtype=tf.float32),
+                          gpu)
 
         tf.summary.scalar(name=log_name + "/v_loss", data=v_loss)
         tf.summary.scalar(name=log_name + "/as_ent", data=as_entropy)
@@ -378,6 +383,10 @@ class AC(tf.keras.Model, Default):
                     #taken_p_log * tf.stop_gradient(advantage) + self.entropy_scale * ent)
 
                 total_loss = 0.5 * v_loss + p_loss
+
+
+            if tf.abs(p_loss) > 5:
+                print('p_loss', p_loss)
 
             grad = tape.gradient(total_loss, self.policy.trainable_variables
                                  + self.V.trainable_variables
